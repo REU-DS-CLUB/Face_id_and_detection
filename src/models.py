1365bef2cee52710102907cfdc6801e4aa9ae9dd
Andrew <suhowandrew@gmail.com>
2023-10-20 01:16:35 +0300

    added vgg4 model

--- src/models.py
+++ src/models.py
@@ -113,3 +113,62 @@ class VGG16(nn.Module):
             out = cur_layer(out)
 
         return out
+
+
+class VGG4(nn.Module):
+    def __init__(self, num_classes, img_size):
+        super(VGG4, self).__init__()
+
+        # Convolutional layers for feature extraction
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1)
+        self.relu1 = nn.ReLU()
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1)
+        self.relu2 = nn.ReLU()
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.relu3 = nn.ReLU()
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # Additional convolutional layer
+        self.conv4 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
+        self.relu4 = nn.ReLU()
+        self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # Fully connected layers for classification
+        self.fc1 = nn.Linear(128 * (img_size // 16) * (img_size // 16), 128)
+        self.relu5 = nn.ReLU()
+        self.fc2 = nn.Linear(128, 64)
+        self.relu6 = nn.ReLU()
+        self.fc3 = nn.Linear(64, num_classes)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu1(x)
+        x = self.pool1(x)
+
+        x = self.conv2(x)
+        x = self.relu2(x)
+        x = self.pool2(x)
+
+        x = self.conv3(x)
+        x = self.relu3(x)
+        x = self.pool3(x)
+
+        # Additional convolutional layer
+        x = self.conv4(x)
+        x = self.relu4(x)
+        x = self.pool4(x)
+
+        # Flatten the data before passing it to fully connected layers
+        x = x.view(x.size(0), -1)
+
+        x = self.fc1(x)
+        x = self.relu5(x)
+        x = self.fc2(x)
+        x = self.relu6(x)
+        x = self.fc3(x)
+
+        return x
