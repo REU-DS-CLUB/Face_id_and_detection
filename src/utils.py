6d411f3a81c6fbfe02d0df1b8710a6263a7f6d1a
Andrew <suhowandrew@gmail.com>
2023-11-03 16:45:42 +0300

    added early stoping

--- src/utils.py
+++ src/utils.py
@@ -578,3 +578,23 @@ def validate_model(model, test_dataloader, loss_fn, device):
     avg_loss = total_loss / len(test_dataloader)  # Средняя потеря за эпоху
     model.train()  # Вернуть модель в режим обучения
     return avg_loss
+
+
+class EarlyStopping:
+    def __init__(self, patience=5, min_delta=0):
+        self.patience = patience
+        self.min_delta = min_delta
+        self.counter = 0
+        self.best_loss = None
+        self.early_stop = False
+    
+    def __call__(self, val_loss):
+        if self.best_loss is None:
+            self.best_loss = val_loss
+        elif val_loss > self.best_loss - self.min_delta:
+            self.counter += 1
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_loss = val_loss
+            self.counter = 0
